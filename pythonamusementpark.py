6ea8f5c1fb6e4e03844a51371613e3caf6bc2c17
dan-te6741 <danieli96@nycstudents.net>
2026-02-03 10:17:19 -0500

    pythonamusementday1

---
+++ pythonamusementpark.py
@@ -0,0 +1,14 @@
+def input (ismember, age, isresident):
+    if (age <= 12):
+        print("Child discount")
+    if (age >= 65):
+        print("Senior discount")
+    if ismember == (True):
+        print("Membership discount")
+    if (isresident == True):
+        print("Resident discount")
+
+input == ismember
+
+
+
