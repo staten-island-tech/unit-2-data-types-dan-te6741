20d3e07f91dad953c26cbb557e2b1d07957ec031
dan-te6741 <danieli96@nycstudents.net>
2026-02-05 10:17:25 -0500

    wordcounter done

---
+++ tipcalculator.py
@@ -0,0 +1,12 @@
+# Use input to make the user input a sentence, then for every i in input print a number
+
+usersentence = input("Word counter")
+list = usersentence.split()
+def wordcount():
+    print(len(list))
+
+wordcount()       
+
+
+
+        
