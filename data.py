8bab72f1d70ac01a3bab0783fbef5086a9a2c3dd
dan-te6741 <danieli96@nycstudents.net>
2026-02-06 10:16:53 -0500

    tipcalc start

---
+++ data.py
@@ -0,0 +1,17 @@
+# Use input to make the user input a sentence, then for every i in input print a number
+
+# usersentence = input("Word counter")
+# list = usersentence.split()
+# def wordcount():
+#     print(len(list))
+
+# wordcount()       
+
+     
+numdet = int(input("Type number"))
+if numdet%2 == 0:
+    print ("Even")
+else:
+    print ("Odd")
+
+
