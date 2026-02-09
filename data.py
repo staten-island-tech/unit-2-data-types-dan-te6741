a200abe94950b8133bc3c9ef058810c6a4297997
dan-te6741 <danieli96@nycstudents.net>
2026-02-09 10:16:59 -0500

    tipcalcdone

--- data.py
+++ data.py
@@ -8,10 +8,24 @@
 # wordcount()       
 
      
-numdet = int(input("Type number"))
-if numdet%2 == 0:
-    print ("Even")
+# numdet = int(input("Type number"))
+# if numdet%2 == 0:
+#     print ("Even")
+# else:
+#     print ("Odd")
+
+base = int(input("Input base bill"))
+
+tipcalc= int(input("Input service quality. 1= bad, 2= okay, 3= good, 4= great"))
+if tipcalc == 1:
+     print(base)
+elif tipcalc == 2: 
+ print (base + base*0.15)
+elif tipcalc == 3:
+    print (base + base*0.20)
+elif tipcalc== 4:
+     print (base + base*0.25)
 else:
-    print ("Odd")
+    print ("Invalid Input")
 
 
