24d4666c715a417121686c881b21f77d89bc0f40
dan-te6741 <danieli96@nycstudents.net>
2026-02-10 10:07:16 -0500

    finished factors starting gcf

--- data.py
+++ data.py
@@ -14,18 +14,26 @@
 # else:
 #     print ("Odd")
 
-base = int(input("Input base bill"))
+# base = int(input("Input base bill"))
 
-tipcalc= int(input("Input service quality. 1= bad, 2= okay, 3= good, 4= great"))
-if tipcalc == 1:
-     print(base)
-elif tipcalc == 2: 
- print (base + base*0.15)
-elif tipcalc == 3:
-    print (base + base*0.20)
-elif tipcalc== 4:
-     print (base + base*0.25)
-else:
-    print ("Invalid Input")
+# tipcalc= int(input("Input service quality. 1= bad, 2= okay, 3= good, 4= great"))
+# if tipcalc == 1:
+#      print(base)
+# elif tipcalc == 2: 
+#  print (base + base*0.15)
+# elif tipcalc == 3:
+#     print (base + base*0.20)
+# elif tipcalc== 4:
+#      print (base + base*0.25)
+# else:
+#     print ("Invalid Input")
 
+x=1
+num = int(input("Input number to calculate all factors of"))
+print(num)
+for i in (range(x,num)):
+    if(num%x == 0):
+        print (x)
+    
+    x = x+1
 
